1eb2cff8a531368c264dd83aab305a5175e256c3
Ilia Ross <ilia@ross.gdn>
2025-03-10 02:43:15 +0200

    Fix to disable Net code temporarily

--- lib/Cloudmin/Config/Plugin/Net.pm
+++ lib/Cloudmin/Config/Plugin/Net.pm
@@ -35,69 +35,69 @@ sub actions {
 
   $self->spin();
   eval {
-    if (foreign_check("net")) {
-      foreign_require("net", "net-lib.pl");
-      my $dns = net::get_dns_config();
-      if (indexof("127.0.0.1", @{$dns->{'nameserver'}}) < 0) {
-        unshift(@{$dns->{'nameserver'}}, "127.0.0.1");
-        net::save_dns_config($dns);
-      }
+    # if (foreign_check("net")) {
+    #   foreign_require("net", "net-lib.pl");
+    #   my $dns = net::get_dns_config();
+    #   if (indexof("127.0.0.1", @{$dns->{'nameserver'}}) < 0) {
+    #     unshift(@{$dns->{'nameserver'}}, "127.0.0.1");
+    #     net::save_dns_config($dns);
+    #   }
 
-      # Force 127.0.0.1 into name servers in resolv.conf
-      # XXX This shouldn't be necessary. There's some kind of bug in net::
-      my $resolvconf = '/etc/resolv.conf';
-      my $rlref      = read_file_lines($resolvconf);
-      if (indexof('nameserver 127.0.0.1'), @{$rlref} < 0) {
-        $log->info("Adding name server 127.0.0.1 to resolv.conf.");
-        unshift(@{$rlref}, 'nameserver 127.0.0.1');
-        unshift(@{$rlref}, '# Added by Cloudmin.');
-      }
-      flush_file_lines($resolvconf);
+    #   # Force 127.0.0.1 into name servers in resolv.conf
+    #   # XXX This shouldn't be necessary. There's some kind of bug in net::
+    #   my $resolvconf = '/etc/resolv.conf';
+    #   my $rlref      = read_file_lines($resolvconf);
+    #   if (indexof('nameserver 127.0.0.1', @{$rlref}) < 0) {
+    #     $log->info("Adding name server 127.0.0.1 to resolv.conf.");
+    #     unshift(@{$rlref}, 'nameserver 127.0.0.1');
+    #     unshift(@{$rlref}, '# Added by Cloudmin.');
+    #   }
+    #   flush_file_lines($resolvconf);
 
-      # On Debian/Ubuntu, if there are extra interfaces files, we need
-      # to update them, too.
-      # Check for additional included config files.
-      my @interfaces_d = glob "/etc/network/interfaces.d/*";
-      if (@interfaces_d) {
+    #   # On Debian/Ubuntu, if there are extra interfaces files, we need
+    #   # to update them, too.
+    #   # Check for additional included config files.
+    #   my @interfaces_d = glob "/etc/network/interfaces.d/*";
+    #   if (@interfaces_d) {
 
-        # Find all of the dns-nameservers entries and update'em
-        foreach my $includefile (@interfaces_d) {
-          open my $fh, "<", $includefile
-            or
-            $log->warning("Failed to open network config file: $includefile.");
-          close $fh;
-        }
-      }
+    #     # Find all of the dns-nameservers entries and update'em
+    #     foreach my $includefile (@interfaces_d) {
+    #       open my $fh, "<", $includefile
+    #         or
+    #         $log->warning("Failed to open network config file: $includefile.");
+    #       close $fh;
+    #     }
+    #   }
 
-      # Check to see if we're configured with dhcp.
-      my @dhcp = grep { $_->{'dhcp'} } net::boot_interfaces();
-      foreach my $includefile (@interfaces_d) {
-        open my $fh, '<', $includefile or die;
-        while (my $line = <$fh>) {
+    #   # Check to see if we're configured with dhcp.
+    #   my @dhcp = grep { $_->{'dhcp'} } net::boot_interfaces();
+    #   foreach my $includefile (@interfaces_d) {
+    #     open my $fh, '<', $includefile or die;
+    #     while (my $line = <$fh>) {
 
-          # This is not smart.
-          if ($line =~ /inet .* dhcp/) {
-            push @dhcp, "1";    # Just stick something truthy in there.
-          }
-        }
-      }
-      if (@dhcp) {
-        $log->warn(
-          "Detected DHCP-configured network. This probably isn't ideal.");
-        my $lref;
-        my $file = '/etc/dhcp/dhclient.conf';
-        if (-e $file) {
-          $lref = read_file_lines($file);
-          if (indexof("prepend domain-name-servers 127.0.0.1;", @{$lref}) < 0) {
-            $log->info(
-              "Attempting to add name server 127.0.0.1 to dhcp configuration.");
-            push(@{$lref}, 'prepend domain-name-servers 127.0.0.1;');
-            push(@{$lref}, '# Added by Cloudmin.');
-          }
-          flush_file_lines($file);
-        }
-      }
-    }
+    #       # This is not smart.
+    #       if ($line =~ /inet .* dhcp/) {
+    #         push @dhcp, "1";    # Just stick something truthy in there.
+    #       }
+    #     }
+    #   }
+    #   if (@dhcp) {
+    #     $log->warn(
+    #       "Detected DHCP-configured network. This probably isn't ideal.");
+    #     my $lref;
+    #     my $file = '/etc/dhcp/dhclient.conf';
+    #     if (-e $file) {
+    #       $lref = read_file_lines($file);
+    #       if (indexof("prepend domain-name-servers 127.0.0.1;", @{$lref}) < 0) {
+    #         $log->info(
+    #           "Attempting to add name server 127.0.0.1 to dhcp configuration.");
+    #         push(@{$lref}, 'prepend domain-name-servers 127.0.0.1;');
+    #         push(@{$lref}, '# Added by Cloudmin.');
+    #       }
+    #       flush_file_lines($file);
+    #     }
+    #   }
+    # }
     $self->done(1);    # OK!
   };
   if ($@) {
